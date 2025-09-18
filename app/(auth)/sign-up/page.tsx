dcb4cd5f223e77f1c0b56960e06903a6f943d8a7
Markus Wiland <markus.wiland@outlook.com>
2025-09-18 15:27:09 +0200

    first commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpForm } from "./_components/sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <SignUpForm />
+    </div>
+  );
+}
